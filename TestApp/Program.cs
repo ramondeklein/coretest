e635a5ebb2ffb7e49f27c8bd2956aaaf5cbf1cf9
Ramon de Klein <mail@ramondeklein.nl>
2018-12-08 01:33:01 +0100

    Initial version

---
+++ TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.ComponentModel;
+using PropertyChanged;
+
+namespace CoreTest
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var initialName = ConfigurationManager.AppSettings["initialName"];
+
+            var person = new Person { Name = initialName };
+            Console.WriteLine($"Person: {person.Name}");
+            person.PropertyChanged += (s, e) => Console.WriteLine($"Name changed to '{person.Name}'");
+            person.Name = "Frank";
+        }
+    }
+}
